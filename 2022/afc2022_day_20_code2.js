236c71ed9d9601067595b82a00c756d6b8a23868
Jean Lacassagne <jean.lacassagne@ledger.com>
2022-12-20 10:32:26 +0100

    day20p2

---
+++ 2022/afc2022_day_20_code2.js
@@ -0,0 +1,48 @@
+with((require('readline')).createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+}))
+{
+    var file = [];
+    
+    on('line', (line) =>
+    {
+	file.push({
+	    x: file.length,
+	    v: parseInt(line) * 811589153
+	});
+    });
+    once('close', () => {
+	for (var i = 0; i < 10; ++i)
+	{
+	    process.stdout.write("\r" + Math.floor(i / 10 * 100) + "%");
+	    for (var x = 0; x < file.length; ++x)
+		for (var s = 0; s < file.length; ++s)
+		    if (file[s].x == x)
+	            {
+			var t = s + (file[s].v % (file.length - 1));
+			if (t < 0)
+			    t += file.length - 1;
+			else if (t >= file.length)
+			    t -= file.length - 1;
+			file.splice(t, 0, file.splice(s, 1)[0]);
+			break;
+		    }
+	}
+	for (var s = 0, ans = 0; s < file.length; ++s)
+	    if (file[s].v == 0)
+	    {
+		for (var count = 1, x = s + 1; count <= 3000; ++count, ++x)
+		{
+		    if (x == file.length)
+			x = 0;
+		    if (count % 1000 == 0)
+			ans += file[x].v;
+		}
+		break;
+	    }
+        process.stdout.write("\r    \r");
+	console.log(ans);
+    });
+};
